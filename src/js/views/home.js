7e3384a4cf35277a44f7f0342d58448576d9a051
Alberto Espinosa <110586852+albertoysaac@users.noreply.github.com>
2024-04-09 05:52:20 +0000

    seccion de personajes funcional y completada

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,7 +1,7 @@
 import React, { useContext } from "react";
-import "../../styles/home.css";
 import { Context } from "../store/appContext";
 import { Card } from "../component/card";
+import "../../styles/home.css";
 
 export const Home = () => {
   const { store, actions } = useContext(Context);
@@ -9,6 +9,7 @@ export const Home = () => {
   return (
     <>
       <div className="container-fluid">
+        <h1 className="characters-title">Characters</h1>
         <div className="row">
           {store.characters.map((character) => {
             console.log(character);
@@ -18,8 +19,9 @@ export const Home = () => {
                 id={character.url}
                 name={character.name}
                 gender={character.gender}
-                hair-color={character.hair_color}
+                hair_color={character.hair_color}
                 eye_color={character.eye_color}
+                birth_year={character.birth_year}
               />
             );
           })}
